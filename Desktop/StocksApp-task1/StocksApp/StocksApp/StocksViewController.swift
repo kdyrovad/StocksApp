e69d9d515c52a0d96521364be88aa0d7d117462a
Dilyara Kdyrova <kdyrovadd@gmail.com>
2022-06-05 03:59:42 +0500

    update task1

---
+++ Desktop/StocksApp-task1/StocksApp/StocksApp/StocksViewController.swift
@@ -0,0 +1,113 @@
+//
+//  StocksViewController.swift
+//  StocksApp
+//
+//  Created by Dilyara on 29.05.2022.
+//
+
+import UIKit
+
+final class StocksViewController: UIViewController {
+    
+    static var isColor: Bool = true
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(StockCell.self, forCellReuseIdentifier: StockCell.typeName)
+        tableView.separatorStyle = .none
+        return tableView
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setUpSubviews()
+        view.backgroundColor = .white
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+    }
+    
+    private func setUpSubviews() {
+        view.addSubview(tableView)
+        tableView.backgroundColor = .clear
+        tableView.allowsSelection = false
+        tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16).isActive = true
+        tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16).isActive = true
+        tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    }
+
+
+}
+
+extension StocksViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: StockCell.typeName, for: indexPath) as? StockCell else {
+            return UITableViewCell()
+        }
+        
+        cell.backgroundColor = StocksViewController.isColor ? UIColor(hexString: "#F0F4F7") : .white
+        
+        cell.layer.cornerRadius = 16
+        
+        StocksViewController.isColor.toggle()
+        
+        return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 68
+    }
+
+}
+
+extension StocksViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView()
+        headerView.backgroundColor = view.backgroundColor
+        return headerView
+    }
+     
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 8
+    }
+    
+}
+
+extension NSObject {
+    static var typeName: String {
+        String(describing: self)
+    }
+}
+
+extension UIColor {
+    convenience init(hexString: String) {
+        let hex = hexString.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int = UInt64()
+        Scanner(string: hex).scanHexInt64(&int)
+        let a, r, g, b: UInt64
+        switch hex.count {
+        case 3: // RGB (12-bit)
+            (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+        case 6: // RGB (24-bit)
+            (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
+        case 8: // ARGB (32-bit)
+            (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+        default:
+            (a, r, g, b) = (255, 0, 0, 0)
+        }
+        self.init(red: CGFloat(r) / 255, green: CGFloat(g) / 255, blue: CGFloat(b) / 255, alpha: CGFloat(a) / 255)
+    }
+}
+
